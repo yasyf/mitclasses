b047f11300926770995a82e197ee37e118f5088c
Yasyf Mohamedali <yasyfm@gmail.com>
2015-11-22 23:17:35 -0500

    Add rails-erd for generating domain models.

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -2,10 +2,10 @@ class Evaluation < ActiveRecord::Base
   CONNECTION = HTTP::Evaluations.new
 
   belongs_to :mit_class
+  has_one :instructor, through: :mit_class
+  has_one :semester, through: :mit_class
 
   after_create :populate!
-
-  delegate :instructor, :semester, to: :mit_class
 
   private
 
