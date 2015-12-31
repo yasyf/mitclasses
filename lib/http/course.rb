57d51efc5ab20fe927e9978b60fc7f5fe68444f3
Yasyf Mohamedali <yasyfm@gmail.com>
2015-12-30 19:29:34 -0800

    Add HTTP::Course#bad_domain? helper.

--- lib/http/course.rb
+++ lib/http/course.rb
@@ -15,14 +15,14 @@ module HTTP
     def potential_links(mit_class)
       links.lazy.
         map(&:uri).
-        reject { |l| BAD_DOMAINS.include?(l.host) }.
+        reject { |l| bad_domain?(l) }.
         reject { |l| check_head(l) }.
         select { |l| verify_link(l, mit_class) }
     end
 
     def check_head(l)
       response = HTTParty.head(l)
-      BAD_DOMAINS.include?(response.request.last_uri.host) || response.code != 200
+      bad_domain?(response.request.last_uri) || response.code != 200
     rescue HTTParty::Error
       true
     end
@@ -31,11 +31,18 @@ module HTTP
       body = HTTParty.get(l).body
       parsed = Nokogiri::HTML(body)
       parsed.xpath("//meta[@http-equiv='refresh' or @http-equiv='REFRESH']").each do |meta|
-        return false if BAD_DOMAINS.include?(URI(meta['content'].split('=').last).host)
+        return false if bad_domain?(meta['content'].split('=').last)
       end
       body.include? mit_class.number
     rescue HTTParty::Error
       false
+    end
+
+    def bad_domain?(uri)
+      uri = URI(uri) unless uri.is_a?(URI)
+      BAD_DOMAINS.include?(uri.host)
+    rescue URI::InvalidURIError
+      true
     end
   end
 end
