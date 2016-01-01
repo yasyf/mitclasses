70628051c6e04c98623fd44dcd2c29ed9f1f9c5b
Yasyf Mohamedali <yasyfm@gmail.com>
2016-01-01 04:17:21 -0800

    Limit Sidekiq connections.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+ENV['WEB_CONCURRENCY'] ||= "2"
+ENV['MAX_THREADS'] ||= "5"
+
 # Initialize the Rails application.
 Rails.application.initialize!
